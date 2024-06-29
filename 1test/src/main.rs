dece7dfdfcac05404c6675a02c4a5902ae921acc
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-29 09:44:40 +0530

    1test init

---
+++ 1test/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    let x:i64=-500000;
+    let y:u32=1000;
+    let z:f32=100.109;
+
+    println!("{}",x);
+    println!("{}", y);
+    println!("{}",z);
+}
