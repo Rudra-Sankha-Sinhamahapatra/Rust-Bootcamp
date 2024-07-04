c555f4ee0c1eeccb9718db565f102a353d5ced57
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-07-04 10:27:31 +0530

    Updating

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,14 +1,32 @@
 fn main() {
-    let s1:String=String::from("Hello");
-    let s2=&s1; //borrows from s1
+    let mut s1: String = String::from("Hello");
+    let s2 = &s1; //borrows from s1
+                  // let s3=& mut s1; //throws an error => Can't have more than one mutable borrower
+    let mut s4 = String::from("Hii");
+    let s5 = &mut s4;
+    // let s6=& s4; 
+    
+    /*A mutable variable can't have both mutable and immutable references at the same
+    time in rust*/
 
-    println!("{}",s2);
-    println!("{}",s1);  //This is valid , the first pointer wasn't invalidated
-  
+    // println!("{}",s6);
+
+    println!("{}", s5);
+
+    println!("{}", s2);
+    println!("{}", s1); //This is valid , the first pointer wasn't invalidated
+
     borrowing(&s1); //passing the reference of s1
-    println!("after{}",s1);
+    println!("after{}", s1);
+
+    update_str(&mut s1);
+    println!("{}", s1);
 }
 
-fn borrowing(some_string:&String){
-println!("{}",some_string);
+fn update_str(word: &mut String) {
+    word.push_str(" World");
+}
+
+fn borrowing(some_string: &String) {
+    println!("{}", some_string);
 }
