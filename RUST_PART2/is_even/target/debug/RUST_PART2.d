255e44183c58903930e646284ca94291925d62c7
Rudra Sankha Sinhamahapatra <101992909+Rudra-Sankha-Sinhamahapatra@users.noreply.github.com>
2024-09-29 16:21:41 +0530

    Delete RUST_PART2/is_even/target directory

--- RUST_PART2/is_even/target/debug/RUST_PART2.d
+++
@@ -1,1 +0,0 @@
-C:\Coding\ by\ Rudra\ -\ Github\Rust-Bootcamp\RUST_PART2\is_even\target\debug\RUST_PART2.exe: C:\Coding\ by\ Rudra\ -\ Github\Rust-Bootcamp\RUST_PART2\is_even\src\main.rs
--- RUST_PART2/is_even/target/debug/deps/RUST_PART2.d
+++
@@ -1,5 +0,0 @@
-C:\Coding by Rudra - Github\Rust-Bootcamp\RUST_PART2\target\debug\deps\RUST_PART2.exe: src/main.rs
-
-C:\Coding by Rudra - Github\Rust-Bootcamp\RUST_PART2\target\debug\deps\RUST_PART2.d: src/main.rs
-
-src/main.rs:
