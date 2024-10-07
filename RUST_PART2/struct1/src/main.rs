ad826451f04a57d11af53e3b63499573b5447a7d
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-07 23:44:09 +0530

    struct program

---
+++ RUST_PART2/struct1/src/main.rs
@@ -0,0 +1,18 @@
+struct User {
+    first_name:String,
+    last_name: String,
+    age: i32
+}
+
+fn main() {
+    let user = User {
+        first_name:String::from("Rudra Sankha"),
+        last_name:String::from("Sinhamahapatra"),
+        age:20
+    };
+
+    println!("{}",user.first_name);
+    println!("{}",user.last_name);
+    println!("{}",user.age);
+}
+
