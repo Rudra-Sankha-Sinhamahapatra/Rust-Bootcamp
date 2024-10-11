dfdec9658fedd520a3eb68295d1a2e61c5ff8f17
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-11 08:23:49 +0530

    adding

--- RUST_PART2/read_toString/src/main.rs
+++ RUST_PART2/read_toString/src/main.rs
@@ -6,8 +6,21 @@ fn main() {
         Ok(data) => println!("{}", data),
         Err(err) => println!("Error while reading the file: {}", err),
     }
+    read2(String::from("a.txt"));
 }
 
+//approach 1
 fn read(filepath: String) -> Result<String, io::Error> {
    return read_to_string(filepath);  // This returns Result<String, io::Error>
+}
+
+//approach 2
+fn read2 (filepath:String) {
+    let res = read_to_string(filepath);
+
+    match res {
+        Ok(data) => println!("{}",data),
+        Err(err) => println!("Error while reading the file {}",err),
+    }
+    
 }
