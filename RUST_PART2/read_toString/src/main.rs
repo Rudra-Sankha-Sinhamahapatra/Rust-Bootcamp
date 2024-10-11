75e2c400043187c072516c69318d013bb09fa401
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-11 08:21:38 +0530

    adding

--- RUST_PART2/read_toString/src/main.rs
+++ RUST_PART2/read_toString/src/main.rs
@@ -1,11 +1,13 @@
 use std::fs::read_to_string;
+use std::io;
 
 fn main() {
-    let res = read_to_string("a.txt");
-
-    match res {
-        Ok(data) => println!("{}",data),
-        Err(err) => println!("Error while reading the file {}",err),
+    match read(String::from("a.txt")) {
+        Ok(data) => println!("{}", data),
+        Err(err) => println!("Error while reading the file: {}", err),
     }
-    
+}
+
+fn read(filepath: String) -> Result<String, io::Error> {
+   return read_to_string(filepath);  // This returns Result<String, io::Error>
 }
