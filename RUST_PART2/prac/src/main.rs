f18b2920ee107d3dbbf18c14df2574ca0a7e9a21
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-11 07:31:54 +0530

    prac

--- RUST_PART2/prac/src/main.rs
+++ RUST_PART2/prac/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
     let ans = iseven(3);
     println!("{}",ans);
+    let p = 5;
+    // let p1 = p;
+    println!("{}",p);
+    let s = String::from("h");
+    let s1 = s;
+    // println!("{}",s);
+    let s2 = &s1;
+    println!("{}",s2);
 }
 
 fn iseven(num:i32) -> bool {
