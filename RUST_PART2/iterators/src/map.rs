71774c655cb607a80a264a7ac8b5d56c6fdf722a
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-15 10:13:29 +0530

    iterator adaptors map

---
+++ RUST_PART2/iterators/src/map.rs
@@ -0,0 +1,13 @@
+
+pub fn map () {
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+
+    let v1_iter2 = v1_iter.map(|x| x+1);
+
+    for i in v1_iter2 {
+        println!("{:?}",i);
+    }
+
+    println!("{:?}",v1);
+}
