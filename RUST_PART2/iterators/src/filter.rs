965ed40a6596afa916d932253da2a7a30d266c9f
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-10-15 10:13:51 +0530

    iterator adapter filter

---
+++ RUST_PART2/iterators/src/filter.rs
@@ -0,0 +1,13 @@
+
+pub fn filter () {
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
+
+    let v1_iter2 = v1_iter.filter(|x| *x%2 ==0);
+
+    for i in v1_iter2 {
+        println!("{:?}",i);
+    }
+
+    println!("{:?}",v1);
+}
